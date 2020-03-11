0cb33e8b80fb44997a51aac6c28c011ed829b877
Lori Ross <laross70@gmail.com>
2020-03-11 15:27:18 -0500

    added html routes and gitignore

---
+++ server.js
@@ -0,0 +1,31 @@
+// create variables for all npm packages being required int
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var app = express();
+
+// set up portal
+var PORT = process.env.PORT || 3000;
+
+// app.use(express.static(__dirname,"public"));
+
+// server handler function test-commented out as no longer needed
+// app.get('/', function(req,res) {
+//     res.send("Hello World");
+// })
+
+
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// require('./develop/routes/apiRoutes.js')(app)
+require('./develop/routes/htmlRoutes.js')(app)
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT:"+ PORT)
+});
+
